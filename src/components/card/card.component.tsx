4db26f7a81b0fc288462678dd53c2a0a33ca32a5
Islam Sayed <islam.sayed8@gmail.com>
2022-11-29 18:10:22 +0200

    dd film prop

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,23 +1,27 @@
 import './card.style.css'
+import {Movie} from '../../types'
+import { Link } from 'react-router-dom'
 
-const Card = () => {
+type Props = {
+  film: Movie,
+}
+
+const Card = ({film}: Props) => {
   return (
-    <div>
-      <a href="/blog/posts/never-stop-learning/" className="latestPosts_card">
+      <Link to={`/cards/${film.id}`} className="latestPosts_card">
         <div className="latestPosts_cardImage">
           <img
-            src="https://image.tmdb.org/t/p/w300/sv1xJUazXeYqALzczSZ3O6nkH75.jpg"
+            src={`https://image.tmdb.org/t/p/w200${film.poster_path}`}
             width="100%"
             alt="project system"
           />
         </div>
 
-        <div className="latestPosts_cardBody">
+        {/* <div className="latestPosts_cardBody">
           <h3 className="latestPosts_cardTitle">Never Stop Learning</h3>
           <p>lorem ipsum dolor sit amet, consectetur adipisicing</p>
-        </div>
-      </a>
-    </div>
+        </div> */}
+      </Link>
   )
 }
 
