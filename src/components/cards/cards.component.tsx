b0d4759cf65a088608d24fa456da800b894e5ed8
Islam Sayed <islam.sayed8@gmail.com>
2022-11-29 18:11:43 +0200

    use axios to make get req

--- src/components/cards/cards.component.tsx
+++ src/components/cards/cards.component.tsx
@@ -1,14 +1,33 @@
 import Card from '../card/card.component'
 import './cards.style.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Movie } from '../../types'
+
 const Cards = () => {
+  const [movies, setMovies] = useState<Movie[]>([])
+  const getNowPlaying = async () => {
+    const { data } = await axios.get(
+      'https://api.themoviedb.org/3/movie/now_playing?api_key=6528ff68dbc27d13fb177793f4c69f9d&language=en-US'
+    )
+    return data.results
+  }
+
+  useEffect(() => {
+    getNowPlaying().then((data) => setMovies(data))
+  }, [])
+
+  useEffect(() => {
+    console.log(movies)
+  }, [movies])
+
   return (
     <section className="latestPosts">
-      <h2 className="latestPosts_title">Posts</h2>
+      <h2 className="latestPosts_title">Now Playing</h2>
       <div className="latestPosts_cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {movies.map((movie) => (
+          <Card key={movie.id} film={movie} />
+        ))}
       </div>
     </section>
   )
