6705063648e8afe00a7f12ddc6eba4846e00df2a
Islam Sayed <islam.sayed8@gmail.com>
2022-12-05 16:43:59 +0200

    add StoriesCards component

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,9 +1,11 @@
 import MoviesCards from '../../components/moviesCards/moviesCards.component'
+import StoriesCards from '../../components/storiesCards/storiesCards.component'
 
 const Home = () => {
   return (
     <>
       <MoviesCards />
+      <StoriesCards />
     </>
   )
 }
