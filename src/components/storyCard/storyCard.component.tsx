313c12c52aaadbf88cc4e510e29a2b1b0d2c99f7
Islam Sayed <islam.sayed8@gmail.com>
2022-12-09 13:28:35 +0200

    add icons

--- src/components/storyCard/storyCard.component.tsx
+++ src/components/storyCard/storyCard.component.tsx
@@ -2,12 +2,14 @@ import './storyCard.style.css'
 import { Story } from '../../types'
 import { Link } from 'react-router-dom'
 import useWindowDimensions from '../../hooks/useWindowDimensions'
-
+import flag from '../../assets/images/flag.png'
+import share from '../../assets/images/share.png'
 type Props = {
   story: Story
 }
 
 const StoryCard = ({ story }: Props) => {
+  console.log(story)
   const { width } = useWindowDimensions()
   return (
     <div className="storyCard">
@@ -17,10 +19,13 @@ const StoryCard = ({ story }: Props) => {
 
       <div className="storyCard_body">
         <h3 className="storyCard_title">{story.title}</h3>
+        <a href={story.url} target="_blank" rel="noreferrer" className="storyCard_source" >{story.source}</a>
+          <img  className="storyCard_flag" src={flag} alt="" />
         <p>{story.excerpt}</p>
-        <a target="_blank" className="storyCard_more" href={story.link}>
+        <a target="_blank" className="storyCard_more" rel="noreferrer" href={story.link}>
           Read Story
         </a>
+          <img  className="storyCard_anotherTab" src={share} alt="" />
       </div>
     </div>
   )
