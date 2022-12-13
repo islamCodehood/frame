e81b1920ab09d3a280c4e04b8ede32c9f0733632
Islam Sayed <islam.sayed8@gmail.com>
2022-12-14 01:30:17 +0200

    add ReviewsCards component

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,10 +1,12 @@
 import MoviesCards from '../../components/moviesCards/moviesCards.component'
+import ReviewsCards from '../../components/reviewsCards/reviewsCards.component'
 import StoriesCards from '../../components/storiesCards/storiesCards.component'
 
 const Home = () => {
   return (
     <>
       <MoviesCards />
+      <ReviewsCards />
       <StoriesCards />
     </>
   )
