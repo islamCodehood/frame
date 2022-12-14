2deb47196ef6dc66c1d8744f89147ee200dc5ffa
Islam Sayed <islam.sayed8@gmail.com>
2022-12-14 09:25:13 +0200

    format code

--- src/components/reviewCard/reviewCard.component.tsx
+++ src/components/reviewCard/reviewCard.component.tsx
@@ -13,22 +13,26 @@ const ReviewCard = ({ film }: Props) => {
     <Link to={`/${film.id}`} className="reviewCard">
       <div className="reviewCard_image">
         <img
-          src={`https://image.tmdb.org/t/p/w200${
-            film.poster_path
-          }`}
+          src={`https://image.tmdb.org/t/p/w200${film.poster_path}`}
           width="100%"
           alt="project system"
         />
       </div>
 
       <div className="reviewCard_body">
-          <div className="reviewCard_user">
-            <div className="reviewCard_user_avatar"></div><span className="reviewCard_user_name">Islam Sayed</span>
-          </div>
-          <h3 className="reviewCard_title">{film.title}</h3><span className="reviewCard_date">{film.release_date.substring(0, 4)}</span>
-          <div className="reviewCard_rate"> ★★★★★ </div>
-          <p className="reviewCard_review">lorem ipsum dolor sit amet, consectetur adipisicing</p>
+        <div className="reviewCard_user">
+          <div className="reviewCard_user_avatar"></div>
+          <span className="reviewCard_user_name">Islam Sayed</span>
         </div>
+        <h3 className="reviewCard_title">{film.title}</h3>
+        <span className="reviewCard_date">
+          {film.release_date.substring(0, 4)}
+        </span>
+        <div className="reviewCard_rate"> ★★★★★ </div>
+        <p className="reviewCard_review">
+          lorem ipsum dolor sit amet, consectetur adipisicing
+        </p>
+      </div>
     </Link>
   )
 }
